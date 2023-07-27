49b9fce750795564555b6c17cfc7dc139e7775bc
Aldiansyah Dwi P <aldiansyhdp@gmail.com>
2023-07-27 19:36:40 +0700

    asdf

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,9 +6,7 @@ const inter = Archivo({ subsets: ['latin'] });
 export default function About() {
   return (
     <PageLayout>
-      <main
-        className={`flex min-h-screen p-24 ${inter.className}`}
-      >
+      <main className={`flex min-h-screen p-24 ${inter.className}`}>
         <div className="px-6 w-full ">
           <div className="flex p-16 flex-col ">
             <div className="rounded-[20px]">
@@ -24,7 +22,7 @@ export default function About() {
                 About Us
               </p>
               <br />
-              
+
               <p
                 className="text-gray-400 text-center"
                 style={{
@@ -34,7 +32,8 @@ export default function About() {
                   // position: 'relative',
                 }}
               >
-                We are group of people who really care about the general masses'
+                We are group of people who really care about the general{' '}
+                {`masses'`}
                 mental health and decided to make a platform on which they could
                 ease their pain by sharing with others.
               </p>
